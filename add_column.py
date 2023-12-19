a0658874421c44f743ebfb0dceaa2151b87747d1
kys0119 <sunnkys119@naver.com>
2023-12-19 17:18:17 +0900

    병상수 기준 분류 컬럼 추가

--- add_column.py
+++ add_column.py
@@ -1,9 +1,11 @@
+#필요한 모듈 임포트
 import pandas as pd
 
-# CSV 파일 읽어오기
+# 필요한 CSV 파일 읽어오기
 df = pd.read_csv('전국의료기관표준데이터.csv', encoding='cp949')
 
 # 의료기관 분류 함수 정의
+# 병상수 기준으로 30개 미만은 '의원', 30~100 병상은 '병원' 100~500 병상은 종합병원, 500 병상 이상은 상급종합병원으로 분류
 def classify_hospital(size):
     if size < 30:
         return '의원'
@@ -16,9 +18,8 @@ def classify_hospital(size):
     else:
         return '분류불가'
 
-# '의료기관분류' 열 추가
+# 병상수 기준으로 분류한 데이터를 새로운 '의료기관분류' 컬럼에 추가
 df['의료기관분류'] = df['병상수'].apply(classify_hospital)
 
-#결과 확인
+#결과 출력
 print(df)
-#print(df[['번호', '병상수', '의료기관분류']])
